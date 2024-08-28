42b706d23329c60ff2d4fff97e92386c55c19526
reyhanardian16 <reyhanardian16@gmail.com>
2024-08-28 16:54:41 +0700

    fix settings

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-gy+r2$shqc5+fg6dx^*(qiu7fkb=@g#-7v^z*g$=ad*vbnxl9%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "muhammad-reyhan32-mentalhealthtracker2.pbp.cs.ui.ac.id."]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "muhammad-reyhan32-mentalhealthtracker2.pbp.cs.ui.ac.id"]
 
 
 # Application definition
