6a9767c54cd22b94ef0413ed1aa5e37a5feb55c5
reyhanardian16 <reyhanardian16@gmail.com>
2024-09-18 10:42:02 +0700

    add view json

--- main/views.py
+++ main/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from main.forms import MoodEntryForm
 from main.models import MoodEntry
+from django.http import HttpResponse
+from django.core import serializers
 
 def show_main(request):
     mood_entries = MoodEntry.objects.all()
@@ -22,3 +24,19 @@ def create_mood_entry(request):
 
     context = {'form': form}
     return render(request, "create_mood_entry.html", context)
+
+def show_xml(request):
+    data = MoodEntry.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = MoodEntry.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = MoodEntry.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json_by_id(request, id):
+    data = MoodEntry.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
