ac5133a030989825953745de1e8b650755edd83f
reyhanardian16 <reyhanardian16@gmail.com>
2024-09-18 07:00:11 +0700

    add moon entry

--- main/views.py
+++ main/views.py
@@ -1,10 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from main.forms import MoodEntryForm
+from main.models import MoodEntry
 
 def show_main(request):
+    mood_entries = MoodEntry.objects.all()
     context = {
-        'npm' : '2306123456',
-        'name': 'Pak Bepe',
-        'class': 'PBP E'
+        'name': 'Muhammad Reyhan Ardian',
+        'class': 'PBP D',
+        'npm': '2306217185',
+        'mood_entries': mood_entries
     }
 
     return render(request, "main.html", context)
+
+def create_mood_entry(request):
+    form = MoodEntryForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return redirect('main:show_main')
+
+    context = {'form': form}
+    return render(request, "create_mood_entry.html", context)
